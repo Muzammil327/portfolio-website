5ede7e54260269e323946a5c3740078f9238784e
Muzammil327 <mmuzammiloff327@gmail.com>
2024-06-05 00:03:51 +0500

    updated: sitemap

--- src/components/MHero/page.tsx
+++ src/components/MHero/page.tsx
@@ -6,6 +6,19 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import Button from '@/components/ui/Button'
 import Container from '../ui/Container'
+import {
+  FaCloudDownloadAlt,
+  FaDownload,
+  FaFacebookF,
+  FaFileDownload,
+  FaInstagram,
+  FaNodeJs,
+  FaPhone,
+  FaReact,
+  FaTwitter,
+  FaYoutube,
+} from 'react-icons/fa'
+import Link from 'next/link'
 
 export default function MHero() {
   const settings = {
@@ -19,10 +32,10 @@ export default function MHero() {
   }
 
   return (
-    <section>
+    <section className="bg-hero-2 relative">
       <Container>
-        <div className="grid md:grid-cols-2 grid-cols-1 gap-4">
-          <div className="lg:pt-40 pt-40 md:text-left text-center">
+        <div className="grid lg:grid-cols-2 grid-cols-1 gap-4">
+          <div className="lg:pt-48 pt-40 lg:text-left text-center">
             <span className="py-2 px-3 rounded-full border-2 border1">
               👋 Welcome to my portfolio
             </span>
@@ -53,19 +66,36 @@ export default function MHero() {
               framework and its ecosystem. I am passionate about creating
               efficient, scalable, and user-friendly web applications.
             </p>
-            <div className="mt-6 flex items-center md:justify-start justify-center gap-4">
-              <Button className="btnIcon px-8 duration-300 ease-in">
-                Hire Me
+            <div className="mt-6 flex items-center lg:justify-start justify-center gap-4">
+              <Button className="btnIcon px-12 duration-300 ease-in rounded-full flex gap-2 items-center">
+                Download CV <FaCloudDownloadAlt size={20} />
               </Button>
-              <Button className="btnIcon px-6 duration-300 ease-in">
-                Download CV
-              </Button>
-              {/* <Button href={'/hireme'} text={''} className={'py-2 px-6'} /> */}
-              {/* <Button href={''} text={'Download CV'} className={'py-2 px-6'} /> */}
+              <ul className="flex gap-4">
+                <li className="btnIcon">
+                  <Link href="/">
+                    <FaFacebookF size={16} />
+                  </Link>
+                </li>
+                <li className="btnIcon">
+                  <Link href="/">
+                    <FaYoutube size={16} />
+                  </Link>
+                </li>
+                <li className="btnIcon">
+                  <Link href="/">
+                    <FaInstagram size={16} />
+                  </Link>
+                </li>
+                <li className="btnIcon">
+                  <Link href="/">
+                    <FaTwitter size={16} />
+                  </Link>
+                </li>
+              </ul>
             </div>
           </div>
-          <div className="md:pt-40 pt-8">
-            <Slider {...settings}>
+          <div className="md:pt-20 pt-8 relative">
+            {/* <Slider {...settings}>
               {SliderImage.map((data: any) => {
                 return (
                   <div className="relative rounded-lg" key={data.id}>
@@ -80,7 +110,51 @@ export default function MHero() {
                   </div>
                 )
               })}
-            </Slider>
+            </Slider> */}
+            <div className="flex pt-12 justify-center">
+              <Image
+                src="/hero_img.jpg"
+                alt=""
+                height={750}
+                width={750}
+                className="h-[520px] w-[520px] rounded-[60px] lg:rotate-12 hover:rotate-0 duration-300 ease-in-out border-8 border-solid hover:border-color2 border-transparent hover:border-opacity-60"
+              />
+            </div>
+            <div className="">
+              <div className="sm:absolute flex items-center sm:gap-4 gap-2 md:mt-0 sm:mt-4 mt-2 md:mx-0 sm:mx-6 mx-3 bg-cyan-50 dark:bg-gray-700 py-3 px-4 rounded-md max-w-[280px] border1 duration-300 ease-in-out 2xl:right-[15rem] xl:right-[10rem] md::right-[7rem] bottom-0">
+                <span className="btnIcon lg:px-4 md:px-3 px-2 lg:py-4 md:py-3 py-2">
+                  <FaPhone />{' '}
+                </span>
+                <div>
+                  <h5 className="text-color2 sm:pb-1 sm:text-base text-xs">
+                    Customer Service
+                  </h5>
+                  <span className="text sm:text-base text-xs">
+                    +92 314 4878266
+                  </span>
+                </div>
+              </div>
+              <div className="sm:absolute flex items-center sm:gap-4 gap-2 md:mt-0 sm:mt-4 mt-2 md:mx-0 sm:mx-6 mx-3 bg-cyan-50 dark:bg-gray-700 py-3 px-4 rounded-md max-w-[280px] border1 duration-300 ease-in-out right-[25rem] bottom-[32rem]">
+                <span className="btnIcon lg:px-4 md:px-3 px-2 lg:py-4 md:py-3 py-2">
+                  <FaReact />{' '}
+                </span>
+                <div>
+                  <span className="text-color2 sm:text-base text-xs">
+                    Frontend Developer
+                  </span>
+                </div>
+              </div>
+              <div className="sm:absolute flex items-center sm:gap-4 gap-2 md:mt-0 sm:mt-4 mt-2 md:mx-0 sm:mx-6 mx-3 bg-cyan-50 dark:bg-gray-700 py-3 px-4 rounded-md max-w-[280px] border1 duration-300 ease-in-out right-[4rem] bottom-[32rem]">
+                <span className="btnIcon lg:px-4 md:px-3 px-2 lg:py-4 md:py-3 py-2">
+                  <FaNodeJs />{' '}
+                </span>
+                <div>
+                  <span className="text-color2 sm:text-base text-xs">
+                    Backend Developer
+                  </span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </Container>
