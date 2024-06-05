212332b1cb57862a816eccaae5169e0351c3ecec
Muzammil327 <mmuzammiloff327@gmail.com>
2024-06-05 15:25:53 +0500

    updated heading

--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -7,9 +7,11 @@ export default function Heading({ head, subhead }: Iprops) {
   return (
     <>
       <div className="text-center my-8 mr-6 relative">
-        <span className="lg:text-[6rem] md:text-[5rem] sm:text-[4rem] text-[2rem] opacity-5 absolute text-center lg:right-[24%] md:right-[16%] right-[28%] md:-top-32 sm:-top-20 -top-12 flex items-center justify-center">
-          {subhead}
-        </span>
+        <div className="flex items-center justify-center">
+          <span className="text xl:text-[8rem] lg:text-[7rem] sm:text-[5rem] text-[4rem] absolute xl:-top-40 lg:-top-32 md:-top-24 sm:-top-20 -top-16 opacity-5">
+            {subhead}
+          </span>
+        </div>
         {/* <span className="text-color2 font-medium text-base">{subhead}</span> */}
         <h3 className="my-4 text font-bold lg:text-3xl text-2xl">{head}</h3>
       </div>
