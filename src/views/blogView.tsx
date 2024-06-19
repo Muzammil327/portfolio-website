ccba616ab477c2fb04ff33f34333335ba7a6a723
Muzammil327 <mmuzammiloff327@gmail.com>
2024-06-19 14:59:47 +0500

    updated 6/19/2024

--- src/views/blogView.tsx
+++ src/views/blogView.tsx
@@ -1,5 +1,3 @@
-'use client'
-import React, { useEffect, useState } from 'react'
 import Container from '@/src/components/ui/Container'
 import BlogCard from '@/src/components/blog/blogCard'
 import PostMetadata from '../components/blog/PostMetadata'
@@ -7,17 +5,17 @@ import { PostCard } from '../types/page'
 import Navbar from '../components/layout/navbar/page'
 import MainHero from '@/src/components/hero/MainHero'
 
-export default function BlogView() {
-  const [postMetadata, setPostMetadata] = useState<PostCard[]>([])
-  console.log('postMetadata:', postMetadata)
-  useEffect(() => {
-    const fetchPostMetadata = async () => {
-      const metadata = await PostMetadata()
-      setPostMetadata(metadata)
-    }
+export default async function BlogView() {
+  const postMetadata = await PostMetadata()
+  // const [postMetadata, setPostMetadata] = useState<PostCard[]>([])
+  // console.log('postMetadata:', postMetadata)
+  // useEffect(() => {
+  //   const fetchPostMetadata = async () => {
+  //     setPostMetadata(metadata)
+  //   }
 
-    fetchPostMetadata()
-  }, [])
+  //   fetchPostMetadata()
+  // }, [])
   return (
     <>
       <header className="bg-hero">
